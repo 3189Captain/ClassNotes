a7a3f125102885995c1667d58ba6107ed5e6dde1
3189Captain <oogabooga300@gmail.com>
2020-01-17 09:52:26 -0700

    Add files via upload

---
+++ Kinematic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kinematic : MonoBehaviour
+{
+	public Vector3 linearVelocity;
+	public float angularVelocity;
+	public GameObject target;
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		//update position and rotation
+		transform.position += linearVelocity * Time.deltaTime;
+		transform.eulerAngles += new Vector3(0, angularVelocity * Time.deltaTime, 0);
+
+		//update linear and angular velocities
+		Seek mySeek = new Seek();
+		mySeek.character = this;
+		mySeek.target = target;
+		SteeringOutput steering = mySeek.getSteering();
+		linearVelocity += steering.linear * Time.deltaTime;
+		angularVelocity += steering.angular * Time.deltaTime;
+    }
+}
